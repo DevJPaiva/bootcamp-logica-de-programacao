852486d3de270c225a6896a309e407f9a800ded7
DevJPaiva <jorgefe0210@gmail.com>
2023-11-29 15:52:55 -0300

    commit desafio realizado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,32 @@
 fun main() {
-    print("Hello World")
-    print("oi")
+    println()
+    println("Classificador Nivél do Heró")
+    println()
+
+    var continuar: String
+
+    do {
+        println("Digite o nome do herói: ")
+        val nome = readlnOrNull()
+
+        println("Digite a XP do Herói: ")
+        var xp = readlnOrNull()?.toIntOrNull()
+
+        var nivel = when {
+            xp!! <= 1000 -> "Ferro"
+            xp <= 2000 -> "Bronze"
+            xp <= 5000 -> "Prata"
+            xp <= 7000 -> "Ouro"
+            xp <= 8000 -> "Platina"
+            xp <= 9000 -> "Ascendente"
+            xp <= 10000 -> "Imortal"
+            else -> "Radiante"
+        }
+
+        println("O herói $nome, tem $xp de experiência e esta no nível $nivel ")
+        println()
+
+        println("Classificar outro Herói ? digite 1 ou qualquer tecla para Sair")
+        continuar = readlnOrNull()?.trim() ?: "0"
+    } while (continuar == "1")
 }
